cd54d1677af0de871092be7e2bc04d54e7fbc0c8
BRIGHT <BRIGHT@DESKTOP-ST51CUU>
2024-01-14 14:38:19 +0300

    Add project files.

---
+++ Var(Keyword)/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+// var keyword'ü aslında bir tip değildir. Sol tarafındaki yazılan tipin tipini alır
+
+
+int a= 10;  
+
+string s= "string";
+
+
+
+// var keyword
+
+
+// integer değişken
+var test = 100;   // var int tipi oldu
+
+
+var stringTest = "string"; // var string tipi oldu
+
+
+// random ile deneyelim
+
+var random = new Random();  // var random tipi oldu
+
+
+var guid = Guid.NewGuid();   // var guid tipi oldu
+
+
+
+
+
+
+var ondalik = 10.9;   // var ondalık tipi oldu
