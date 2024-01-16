5990ded652519110844d68d136326c02f02b506a
BRIGHT <BRIGHT@DESKTOP-ST51CUU>
2024-01-16 19:20:47 +0300

    ufak tekrar

--- Var(Keyword)/Program.cs
+++ Var(Keyword)/Program.cs
@@ -3,12 +3,11 @@
 
 // var keyword'ü aslında bir tip değildir. Sol tarafındaki yazılan tipin tipini alır
 
+using System.Collections;
 
 int a= 10;  
 
 string s= "string";
-
-
 
 // var keyword
 
@@ -22,14 +21,17 @@ var stringTest = "string"; // var string tipi oldu
 
 // random ile deneyelim
 
+
+Random rnd = new Random();
+
 var random = new Random();  // var random tipi oldu
 
 
 var guid = Guid.NewGuid();   // var guid tipi oldu
 
 
-
-
-
-
 var ondalik = 10.9;   // var ondalık tipi oldu
+
+var testa = 'A';
+
+
